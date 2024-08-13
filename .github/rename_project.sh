90dfc9b95d08a4d10c1178ddc253ffb2b489d806
justin-a-nobody <justin-a-nobody@users.noreply.github.com>
2024-08-13 00:33:07 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="justin-a-nobody"
+original_name="py_project"
+original_urlname="py_project"
+original_description="Awesome py_project created by justin-a-nobody"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv py_project $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
